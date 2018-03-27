8af007eabb7649ff84e0835e3ff0960427781d3b
Collin <aevitus23@gmail.com>
2018-03-26 21:56:39 -0400

    Added cell_products

--- sde2.ml
+++ sde2.ml
@@ -24,3 +24,26 @@ Signature: val get_table_values_cell : int list * ’a list list -> ’a = <fun>
 *)
 let get_table_values_cell ([i;j],table) =
 	List.nth (List.nth table (j-1)) (i-1);; 
+
+(**
+Prototype: cell_products [cell1;cell2]
+Input(s): list containing 2 cells
+Returned Value: resultant list of strings
+Side Effects: none
+Signature: val cell_products : string list list -> string list = <fun>
+*)
+let rec cell_products_helper (item, cell2) =
+	if cell2 == [] then
+		[]
+	else
+		List.append [String.concat "" (List.append [item] [List.hd cell2])] (cell_products_helper(item, (List.tl cell2)));;
+
+let rec cell_products_rec ([cell1;cell2]) = 
+	if cell1 == [] then
+		[]
+	else
+		List.append (cell_products_helper((List.hd cell1), cell2)) (cell_products([(List.tl cell1);cell2]));;
+
+let cell_products ([cell1;cell2]) = cell_products_rec ([cell1;cell2]);;
+
+
